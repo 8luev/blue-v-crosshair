2f6d57e625c8bd7b79f538b964badc6836327036
8luev <imcvqm@gmail.com>
2025-05-10 16:01:53 +1200

    Added a tray that when clicked opens a menu.

--- index.js
+++ index.js
@@ -1,26 +1,57 @@
 // this ass code was produced by @8leuv
 
-const {BrowserWindow, app, screen} = require('electron');
+const {BrowserWindow, app, Tray} = require('electron');
 
 function Client() {
     const client = new BrowserWindow({
-        fullscreen: true, //-- Fullscreen it so the crosshair defaults to center on any resolution.
+        fullscreen: true, // Fullscreen it so the crosshair defaults to center on any resolution.
         autoHideMenuBar: true,
         resizable: false,
         minimizable: false,
         alwaysOnTop: true,
         focusable: false,
         transparent: true,
-        frame: false, //-- Removes the close, minimize etc bar at the top
+        frame: false, // Removes the close, minimize etc bar at the top
         webPreferences: {
             devTools: false,
         }
     });
     client.loadFile('./src/index.html/');
-    client.setIgnoreMouseEvents(true);
+    client.setIgnoreMouseEvents(true); // Ignore mouse events (duh)
 };
+
+function MenuTray() {
+    const menu = new BrowserWindow({
+        width: 400,
+        height: 500,
+        autoHideMenuBar: true,
+        resizable: false,
+        show: false, // Start without it showing
+        icon: './src/assets/favicon.png'
+    });
+
+    menu.loadFile('./src/menu/menu.html');
+
+    menu.on('blur', () => {
+        if(menu.isVisible()) {
+            menu.hide(); // If menu loses focus it hides it again
+        }
+    });
+
+    tray = new Tray('./src/assets/favicon.png');
+    tray.setToolTip('Crosshair application.');
+
+    tray.on('click', () => {
+        if(menu.isVisible()) {
+            menu.hide();
+        } else {
+            menu.show();
+        }
+    });
+}
 
 
 app.on('ready', () => {
     Client();
+    MenuTray();
 });
