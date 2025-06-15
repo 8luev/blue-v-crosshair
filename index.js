572ce44db884919b47ce52142f7962ed85184a55
= <=>
2025-06-15 13:55:05 +1200

    remade it to only be an image overlay.

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// this ass code was produced by @8leuv
-
 const {BrowserWindow, app, Tray} = require('electron');
 
 function Client() {
@@ -20,43 +18,6 @@ function Client() {
     client.setIgnoreMouseEvents(true); // Ignore mouse events (duh)
 };
 
-function MenuTray() {
-    const menu = new BrowserWindow({
-        width: 400,
-        height: 500,
-        autoHideMenuBar: true,
-        resizable: false,
-        show: false, // Start without it showing
-        icon: './src/assets/favicon.png'
-    });
-
-    menu.loadFile('./src/menu/menu.html');
-
-    menu.on('blur', () => {
-        if(menu.isVisible()) {
-            menu.hide(); // If menu loses focus it hides it again
-        }
-    });
-
-    menu.on('close', (event) => {
-        event.preventDefault();
-        menu.hide();
-    });
-    
-    tray = new Tray('./src/assets/favicon.png');
-    tray.setToolTip('Crosshair application.');
-
-    tray.on('click', () => {
-        if(menu.isVisible()) {
-            menu.hide();
-        } else {
-            menu.show();
-        }
-    });
-}
-
-
 app.on('ready', () => {
     Client();
-    MenuTray();
 });
